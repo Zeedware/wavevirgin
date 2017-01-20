7db69a47b4753c89d412b067a52ef42fdbc5d22c
Laciel\Rismanto <rismanto.touchten@gmail.com>
2017-01-20 21:47:42 +0700

    ui

---
+++ Assets/script(manto)/GameEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEvent {
+
+	public delegate void OnAddScore_Handler(int score);
+	public static event OnAddScore_Handler onAddScoreE;
+	public static void OnAddScore(int score)
+	{
+		if (onAddScoreE != null)
+			onAddScoreE (score);		
+	}
+
+	public delegate void OnEartShake_Handle();
+	public static event OnEartShake_Handle onEarthShakeE;
+	public static void OnEarthShake()
+	{
+		if (onEarthShakeE != null)
+			onEarthShakeE ();		
+	}
+
+	public delegate void OnTouchPeople_Handle(bool isRight);
+	public static event OnTouchPeople_Handle onTouchPeopleE;
+	public static void OnTouchPeople(bool isRight)
+	{
+		if (onTouchPeopleE != null)
+			onTouchPeopleE (isRight);
+	}
+}
---
+++ Assets/script(manto)/GameplayUIController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameplayUIController : MonoBehaviour {
+
+	public Text scoreText;
+	public int currentScore;
+	public GameObject particleContainer;
+	public ParticleSystem touchParticle_Right, touchParticle_False;
+	public Button skill_earthShake;
+
+	void OnEnable()
+	{
+		GameEvent.onAddScoreE += GameEvent_onAddScoreE;
+		GameEvent.onEarthShakeE += GameEvent_onEarthShakeE;
+	}
+
+
+	void OnDisable()
+	{
+		GameEvent.onAddScoreE -= GameEvent_onAddScoreE;
+		GameEvent.onEarthShakeE -= GameEvent_onEarthShakeE;
+
+	}
+
+	void Start()
+	{
+		skill_earthShake.onClick.AddListener (ActivateSkill_1);
+	}
+
+	void GameEvent_onEarthShakeE ()
+	{
+		Debug.Log ("SKILL EARTH SHAKE");
+	}
+
+	void ActivateSkill_1()
+	{
+		GameEvent.OnEarthShake ();
+	}
+
+	void GameEvent_onAddScoreE (int score)
+	{
+		currentScore += score;
+		scoreText.text = "Score : " + currentScore.ToString ();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.A)) {
+			GameEvent.OnAddScore (1);
+			StartCoroutine (PlayParticle (Random.Range(0,1) < 0.5f));
+		}
+	}
+
+	IEnumerator PlayParticle(bool isRight)
+	{
+		Vector3 particlePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		particleContainer.transform.position = new Vector3 (particlePosition.x, particlePosition.y, 0);
+		particleContainer.SetActive (true);
+		ParticleSystem particle;
+
+		if (isRight)
+			particle = touchParticle_Right;
+		else
+			particle = touchParticle_False;
+
+		particle.Play ();
+		yield return new WaitForSeconds (1);
+		particle.Stop ();
+		particleContainer.SetActive (false);
+	}
+}
